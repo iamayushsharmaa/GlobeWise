0f337fedd065f6f620677cec723fcae7463a1560
Ayush Sharma <165563915+iamayushsharmaa@users.noreply.github.com>
2024-12-26 20:07:30 +0530

    Screens

--- app/src/main/java/com/example/globewise/view/home/Home.kt
+++ app/src/main/java/com/example/globewise/view/home/Home.kt
@@ -2,11 +2,17 @@ package com.example.globewise.view.home
 
 import android.net.wifi.hotspot2.pps.HomeSp
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Home() {
 
-    Column {  }
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+
+    }
 }
